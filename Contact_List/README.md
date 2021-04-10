edd8cf8045207f702b37212d46d9702bb3bd6188
LeonardoBalestere <alvesleonardo92@hotmail.com>
2021-04-10 14:33:05 -0300

    Remove unfunctional project

--- Contact_List/README.md
+++
@@ -1,5 +0,0 @@
-#  Contact_list
-
-This app will take the contacts from the user's Google account.
-
-
--- Contact_List/app/src/main/java/com/example/recylerview/Contact.kt
+++
@@ -1,7 +0,0 @@
-package com.example.recylerview
-
-data class Contact(
-    var name:String,
-    var phone: String,
-    var photograph: String
-)
